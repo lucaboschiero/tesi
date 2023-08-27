8dd339e723cd77ad0c753d6d82a7820c7dd03b08
lucaboschiero <luca.boschiero@studenti.unitn.it>
2023-08-28 00:43:20 +0200

    esecuzione con bpic2011

--- settings.py
+++ settings.py
@@ -72,13 +72,13 @@ datasets_names = ["bpic2011_f1", "bpic2011_f2", "bpic2011_f3", "bpic2011_f4",
 datasets_names = ["Production"]
 """
 """
-datasets_names = ["bpic2011_f3", "bpic2011_f4",
+datasets_names = ["bpic2011_f4",
                   "bpic2012_cancelled", "bpic2012_declined",
                   "bpic2017_accepted", "bpic2017_cancelled",
                   "bpic2017_refused"]
 """
 #"bpic2012_cancelled", "bpic2012_declined"
-datasets_names = ["bpic2011_f3"]
+datasets_names = ["bpic2011_f4"]
 # ================ hyperparameters ================
 """
 hyperparameters = {'support_threshold': [support_threshold_dict['min']-0.2, support_threshold_dict['min']-0.1,
